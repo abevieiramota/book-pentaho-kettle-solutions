7c500f847503d7b52e1d93bc166dbe46d5fdbcdf
Abelardo Vieira Mota <abevieiramota@gmail.com>
2016-05-09 19:40:34 -0400

    Início da migração do Sakila para o Pagila
    Iniciado script de criação do banco(pagila_dwh_schema.sql), iniciando
    pela dim_date(seguindo a ordem do livro), usando como base o script de
    criação do banco na versão sakila.
    Finalizada a criação da tabela dim_date(finalizada meio que uma pinoia,
    pq só fiz criar uma tabela com uma estrutura que mais ou menos adere à
    estrutura da versão sakila e que permitiu executar o load_dim_date.ktr -
    meu primeiro comentário livro no github!)

--- book-material-pagila-version/635179_code_ch04/create_sakila_accounts.sql
+++
@@ -1,5 +0,0 @@
-CREATE USER sakila IDENTIFIED BY 'sakila';
-GRANT ALL PRIVILEGES ON sakila.* TO sakila;
-
-CREATE USER sakila_dwh IDENTIFIED BY 'sakila_dwh';
-GRANT ALL PRIVILEGES ON sakila_dwh.* TO sakila_dwh;
--- pagila-database/README.md
+++ pagila-database/README.md
@@ -1,1 +1,2 @@
 fonte: http://www.postgresqltutorial.com/postgresql-sample-database/
+tutorial: http://www.postgresqltutorial.com/load-postgresql-sample-database/
